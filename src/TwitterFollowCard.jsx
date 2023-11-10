5987945900526ba8688deedf470d06fb660a96cd
jhoanwoa <jhoan19orozco@hotmail.com>
2023-11-10 02:31:26 -0500

    Update: add "dejar de seguir al usuario"

--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react"; 
+import { useState } from "react";
 
-export function TwitterFollowCard({ children, userName, initialIsFollowing}) {
-const [ isFollowing, setIsFollowing] = useState(initialIsFollowing)
+export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
 
-const text = isFollowing ? "Siguiendo" : "Seguir" 
-const buttonClassName = isFollowing ? "tw-followCard-button is--followin" : "tw-followCard-button"
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
 
-function handlerClick () {
-    setIsFollowing( !isFollowing )
-}
+  const handlerClick = () => {
+    setIsFollowing(!isFollowing);
+  };
 
-return (
+  return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
         <img
@@ -24,7 +26,10 @@ return (
         </div>
       </header>
       <aside>
-        <button className={buttonClassName} onClick={handlerClick}>{text}</button>
+        <button className={buttonClassName} onClick={handlerClick}>
+        <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+        </button>
       </aside>
     </article>
   );
